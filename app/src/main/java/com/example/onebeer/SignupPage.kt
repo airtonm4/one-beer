4a2cc08ba063cb31845c74f0f7e2a71cef306384
airtonm4 <airton.atmi@gmail.com>
2023-05-21 19:27:35 -0300

    Added sign up page.

--- app/src/main/java/com/example/onebeer/SignupPage.kt
+++ app/src/main/java/com/example/onebeer/SignupPage.kt
@@ -6,11 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.example.onebeer.databinding.LoginPageBinding
+import com.example.onebeer.databinding.SingupPageBinding
 
-class LoginPage : Fragment() {
+class SignupPage: Fragment() {
 
-    private var _binding: LoginPageBinding? = null
+    private var _binding: SingupPageBinding? = null
 
     private val binding get() = _binding!!
 
@@ -19,15 +19,18 @@ class LoginPage : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = LoginPageBinding.inflate(inflater, container, false)
+        _binding = SingupPageBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.signupTextNavigate.setOnClickListener {
-            findNavController().navigate(R.id.action_login_to_sign_up)
+        binding.buttonSignup.setOnClickListener{
+            findNavController().navigate(R.id.action_sign_up_to_login)
+        }
+        binding.loginTextNavigate.setOnClickListener {
+            findNavController().navigate(R.id.action_sign_up_to_login)
         }
     }
 
