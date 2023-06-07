260dfb03871421a8b61d16cff0c87fe5ba115398
airtonm4 <airton.atmi@gmail.com>
2023-06-07 07:49:45 -0300

    Fix errors.

--- app/src/main/java/com/example/onebeer/BeerCarousel/Beer.kt
+++ app/src/main/java/com/example/onebeer/BeerCarousel/Beer.kt
@@ -2,4 +2,4 @@ package com.example.onebeer.BeerCarousel
 
 import android.media.Image
 
-class Beer (val title: String, val price: String)
+class Beer (val title: String, val price: String, val ml: Number, val description: String, val style: String, val imageUrl: String)
--- app/src/main/java/com/example/onebeer/Profile/ProfilePage.kt
+++ app/src/main/java/com/example/onebeer/Profile/ProfilePage.kt
@@ -3,14 +3,19 @@ package com.example.onebeer.Profile
 import android.content.Intent
 import androidx.fragment.app.Fragment
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.onebeer.BeerCarousel.Beer
+import com.example.onebeer.MainActivity
 
 import com.example.onebeer.databinding.ProfilePageBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 
 
 class ProfilePage: Fragment() {
@@ -27,6 +32,27 @@ class ProfilePage: Fragment() {
         savedInstanceState: Bundle?
 
     ): View? {
+
+//        val db = Firebase.firestore
+//        val beer = hashMapOf(
+//            "title" to "Olaria",
+//            "ml" to 800,
+//            "price" to 42,
+//            "style" to "APA Pale Ale",
+//            "description" to "Cerveja originária de Senador Canedo - GO"
+//
+//        )
+//
+//        db.collection("beers")
+//            .add(beer)
+//            .addOnSuccessListener { documentReference ->
+//                Log.d("Beer add", "DocumentSnapshot added with ID: ${documentReference.id}")
+//                Log.d("Beer add", documentReference.toString())
+//            }.addOnFailureListener { exception ->
+//                Log.d("failure", exception.toString())
+//            }
+
+        val storage = Firebase.storage.reference
 
         auth = Firebase.auth
         val user = auth.currentUser
